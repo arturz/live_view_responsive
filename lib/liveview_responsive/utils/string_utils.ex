ebf35c4c852eb144413c2d68c33b8265b21b4e66
Artur Ziętkiewicz <artur.zietkiewicz@curiosum.com>
2024-07-30 19:08:17 +0200

    Add utility for strings manipulation

---
+++ lib/liveview_responsive/utils/string_utils.ex
@@ -0,0 +1,15 @@
+defmodule LiveviewResponsive.Utils.StringUtils do
+  @moduledoc false
+
+  @spec atom_to_string(atom()) :: String.t()
+  @spec atom_to_string(arg) :: arg when arg: var
+  def atom_to_string(value) when is_atom(value) and not is_boolean(value),
+    do: Atom.to_string(value)
+
+  def atom_to_string(value), do: value
+
+  @spec replace_underscores_with_hyphens(String.t()) :: String.t()
+  def replace_underscores_with_hyphens(string) do
+    String.replace(string, "_", "-")
+  end
+end
