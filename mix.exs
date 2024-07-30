1a3649fd36ebd94bb15fa457e30691c890c9e047
Artur Ziętkiewicz <artur.zietkiewicz@curiosum.com>
2024-07-30 19:09:34 +0200

    Update elixirc_paths

--- mix.exs
+++ mix.exs
@@ -6,10 +6,14 @@ defmodule LiveviewResponsive.MixProject do
       app: :liveview_responsive,
       version: "0.1.0",
       elixir: "~> 1.16",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help compile.app" to learn about applications.
   def application do
