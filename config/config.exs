c143352fcff33f5575b35ef7c4e99da8276cba57
Artur Ziętkiewicz <artur.zietkiewicz@curiosum.com>
2024-08-01 22:24:49 +0200

    Create config.exs file

---
+++ config/config.exs
@@ -0,0 +1,5 @@
+import Config
+
+config :live_view_responsive, LiveViewResponsive.Endpoint,
+  live_view: [signing_salt: "YzJRBmHq"],
+  secret_key_base: "wdSjRlkt/z/ekmHxGHQ6IQ1SFeM2Gr5CiXmYN2Rjooa7/bSSslm8xD4+0SoJDsWV"
