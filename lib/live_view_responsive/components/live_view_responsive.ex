d7af891fe159634960bf7e45637cd51b521c5f2b
Artur Ziętkiewicz <artur.zietkiewicz@curiosum.com>
2024-08-05 16:19:10 +0200

    Require myself attribute in live_view_responsive component

--- lib/live_view_responsive/components/live_view_responsive.ex
+++ lib/live_view_responsive/components/live_view_responsive.ex
@@ -1,10 +1,37 @@
 defmodule LiveViewResponsive.Components.LiveViewResponsive do
-  @moduledoc false
+  @moduledoc """
+  Module containing `<.live_view_responsive>` component.
+  """
 
-  import Phoenix.Component
+  use Phoenix.Component
 
+  attr(:myself, :any, required: true, doc: "@myself assign from the parent component")
+
+  @doc """
+  Renders `LiveViewResponsiveHook` hook required for `LiveViewResponsive.Core.assign_media_query/3`.
+
+  ## Example
+
+  ```elixir
+  def render(assigns) do
+    ~H\"\"\"
+    <div>
+      <.live_view_responsive myself={@myself} />
+      ...
+    </div>
+    \"\"\"
+  end
+  ```
+  """
   def live_view_responsive(assigns) do
-    id = "liveview-responsive-#{:erlang.unique_integer([:positive])}"
+    unless Map.has_key?(assigns, :myself) do
+      raise """
+      Required `:myself` assign is missing. It should be set to `@myself` in the parent component.
+      Example: `<.live_view_responsive myself={@myself} />`
+      """
+    end
+
+    id = "live-view-responsive-#{assigns.myself.cid}"
     assigns = Map.put(assigns, :id, id)
 
     ~H"""
