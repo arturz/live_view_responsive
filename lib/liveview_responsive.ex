b75b7ed4dda7706a4cbebddb978b158bca0bccd8
Artur Ziętkiewicz <artur.zietkiewicz@curiosum.com>
2024-08-05 16:10:42 +0200

    Create moduledoc for LiveViewResponsive module

--- lib/liveview_responsive.ex
+++ lib/liveview_responsive.ex
@@ -1,6 +1,76 @@
 defmodule LiveViewResponsive do
   @moduledoc """
-  Documentation for `LiveViewResponsive`.
+  Media queries for responsive design in Phoenix LiveView.
+
+  ## Example
+
+  ### With `<.media_query>` component
+
+  ```elixir
+  defmodule ExampleAppWeb.Example do
+    use Phoenix.Component
+
+    import LiveViewResponsive
+
+    def example(assigns) do
+      ~H\"\"\"
+      <.media_query max_width={1224}>
+        <p>You are on a tablet or mobile</p>
+      </.media_query>
+      <.media_query min_width={1225}>
+        <p>You are on a desktop or laptop</p>
+        <.media_query min_width={1500}>
+          <p>You also have a huge screen</p>
+        </.media_query>
+      </.media_query>
+      \"\"\"
+    end
+  end
+  ```
+
+  ### With media query assigns
+
+  ```elixir
+  defmodule ExampleAppWeb.ExampleComponent do
+    use ExampleAppWeb, :live_component
+
+    use LiveViewResponsive
+
+    def mount(socket) do
+      socket =
+        socket
+        |> assign_media_query(:tablet_or_mobile, max_width: "1224px")
+        |> assign_media_query(:desktop_or_laptop, min_width: "1225px")
+        |> assign_media_query(:portrait, orientation: "portrait")
+
+      {:ok, socket}
+    end
+
+    @impl true
+    def render(assigns) do
+      ~H\"\"\"
+      <div>
+        <.live_view_responsive myself={@myself} />
+
+        <div :if={@live_view_responsive_synced}>
+          <h1>Device test</h1>
+          <p :if={@desktop_or_laptop}>
+            You are on a desktop or laptop
+          </p>
+          <p :if={@tablet_or_mobile}>
+            You are on a tablet or mobile phone
+          </p>
+          <p>
+            You are in
+            <%= if assigns.portrait, do: "portrait", else: "landscape" %>
+            orientation
+          </p>
+        </div>
+      </div>
+      \"\"\"
+    end
+  end
+  ```
   """
 
   defdelegate assign_media_query(socket, name, opts),
@@ -18,7 +88,7 @@ defmodule LiveViewResponsive do
       Event handler for client-side media queries change.
       Runs `update` inside the component's module, so derived assigns can be calculated.
       """
-      def handle_event("liveview-responsive-change", params, socket) do
+      def handle_event("live-view-responsive-change", params, socket) do
         LiveViewResponsive.Core.live_view_responsive_change_event_handler(socket, params)
       end
 
