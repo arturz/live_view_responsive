669c50a62865ff22a7cc996bb51dceac7219b6ed
Artur Ziętkiewicz <artur.zietkiewicz@curiosum.com>
2024-08-05 16:17:09 +0200

    Update docs in MediaQuery module

--- lib/live_view_responsive/components/media_query.ex
+++ lib/live_view_responsive/components/media_query.ex
@@ -1,13 +1,17 @@
 defmodule LiveViewResponsive.Components.MediaQuery do
-  @moduledoc false
+  @moduledoc """
+  Module containing `<.media_query>` component.
+  """
 
   use Phoenix.Component
 
   alias LiveViewResponsive.OptsParser
   alias LiveViewResponsive.QueryBuilder
 
+  @string_or_number_doc "String or number. When number is passed, suffix `px` is added."
+
   # custom query
-  attr(:query, :string)
+  attr(:query, :string, doc: "Custom media query. When used, other attributes are ignored.")
 
   # media types
   attr(:all, :boolean)
@@ -27,34 +31,34 @@ defmodule LiveViewResponsive.Components.MediaQuery do
   attr(:scan, :any, values: [:progressive, :interlace, "progressive", "interlace"])
 
   # aspect ratio
-  attr(:aspect_ratio, :any)
-  attr(:min_aspect_ratio, :any)
-  attr(:max_aspect_ratio, :any)
+  attr(:aspect_ratio, :any, doc: @string_or_number_doc)
+  attr(:min_aspect_ratio, :any, doc: @string_or_number_doc)
+  attr(:max_aspect_ratio, :any, doc: @string_or_number_doc)
 
   # device aspect ratio
-  attr(:device_aspect_ratio, :any)
-  attr(:min_device_aspect_ratio, :any)
-  attr(:max_device_aspect_ratio, :any)
+  attr(:device_aspect_ratio, :any, doc: @string_or_number_doc)
+  attr(:min_device_aspect_ratio, :any, doc: @string_or_number_doc)
+  attr(:max_device_aspect_ratio, :any, doc: @string_or_number_doc)
 
   # height
-  attr(:height, :any)
-  attr(:min_height, :any)
-  attr(:max_height, :any)
+  attr(:height, :any, doc: @string_or_number_doc)
+  attr(:min_height, :any, doc: @string_or_number_doc)
+  attr(:max_height, :any, doc: @string_or_number_doc)
 
   # device height
-  attr(:device_height, :any)
-  attr(:min_device_height, :any)
-  attr(:max_device_height, :any)
+  attr(:device_height, :any, doc: @string_or_number_doc)
+  attr(:min_device_height, :any, doc: @string_or_number_doc)
+  attr(:max_device_height, :any, doc: @string_or_number_doc)
 
   # width
-  attr(:width, :any)
-  attr(:min_width, :any)
-  attr(:max_width, :any)
+  attr(:width, :any, doc: @string_or_number_doc)
+  attr(:min_width, :any, doc: @string_or_number_doc)
+  attr(:max_width, :any, doc: @string_or_number_doc)
 
   # device width
-  attr(:device_width, :any)
-  attr(:min_device_width, :any)
-  attr(:max_device_width, :any)
+  attr(:device_width, :any, doc: @string_or_number_doc)
+  attr(:min_device_width, :any, doc: @string_or_number_doc)
+  attr(:max_device_width, :any, doc: @string_or_number_doc)
 
   # color
   attr(:color, :boolean)
@@ -72,16 +76,45 @@ defmodule LiveViewResponsive.Components.MediaQuery do
   attr(:max_monochrome, :integer)
 
   # resolution
-  attr(:resolution, :any)
-  attr(:min_resolution, :any)
-  attr(:max_resolution, :any)
+  attr(:resolution, :any, doc: @string_or_number_doc)
+  attr(:min_resolution, :any, doc: @string_or_number_doc)
+  attr(:max_resolution, :any, doc: @string_or_number_doc)
 
-  attr(:rest, :any)
+  attr(:rest, :any, doc: @string_or_number_doc)
 
+  @doc """
+  A component which shows or hides children on the front-end side based on a media query.
+
+  Uses the `LiveViewResponsiveMediaQueryHook` hook.
+
+  ## Example
+
+  ```elixir
+  defmodule ExampleAppWeb.Example do
+    use Phoenix.Component
+
+    import LiveViewResponsive
+
+    def example(assigns) do
+      ~H\"\"\"
+      <.media_query max_width={1224}>
+        <p>You are on a tablet or mobile</p>
+      </.media_query>
+      <.media_query min_width={1225}>
+        <p>You are on a desktop or laptop</p>
+        <.media_query min_width={1500}>
+          <p>You also have a huge screen</p>
+        </.media_query>
+      </.media_query>
+      \"\"\"
+    end
+  end
+  ```
+  """
   def media_query(assigns) do
     query = assigns |> assigns_to_attributes() |> OptsParser.parse() |> QueryBuilder.build()
 
-    id = "liveview-responsive-media-query-#{:erlang.unique_integer([:positive])}"
+    id = "live-view-responsive-media-query-#{:erlang.unique_integer([:positive])}"
 
     assigns = assign(assigns, query: query, id: id)
 
