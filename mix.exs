f415c6e50eafa219862781b7c0a7b8f30f56afb0
Artur Ziętkiewicz <artur.zietkiewicz@curiosum.com>
2024-08-05 16:12:49 +0200

    Add package, docs and Credo to mix.exs

--- mix.exs
+++ mix.exs
@@ -7,8 +7,13 @@ defmodule LiveViewResponsive.MixProject do
       version: "0.1.0",
       elixir: "~> 1.16",
       elixirc_paths: elixirc_paths(Mix.env()),
+      elixirc_options: [
+        warnings_as_errors: true
+      ],
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      docs: docs()
     ]
   end
 
@@ -28,9 +33,27 @@ defmodule LiveViewResponsive.MixProject do
     [
       {:phoenix_live_view, ">= 0.18.0"},
       {:phoenix, ">= 1.0.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
 
       # Phoenix LiveView requires Floki as a test dependency.
       {:floki, ">= 0.30.0", only: :test}
+    ]
+  end
+
+  defp package do
+    [
+      files: ~w(lib lib_client) ++ ~w(mix.exs package.json LICENSE README.md .formatter.exs),
+      maintainers: ["Artur Ziętkiewicz"],
+      links: %{
+        "GitHub" => "https://github.com/arturz/live_view_responsive"
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      main: "LiveViewResponsive"
     ]
   end
 
