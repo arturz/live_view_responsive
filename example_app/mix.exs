d1365a3010db89b76136e73f46744a4891625ca9
Artur Ziętkiewicz <artur.zietkiewicz@curiosum.com>
2025-04-25 15:46:33 +0200

    Install Wallaby in example_app

--- example_app/mix.exs
+++ example_app/mix.exs
@@ -37,6 +37,7 @@ defmodule ExampleApp.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.20.2"},
       {:floki, ">= 0.30.0", only: :test},
+      {:wallaby, "~> 0.30.10", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.2", runtime: Mix.env() == :dev},
